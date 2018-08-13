7fa9cc91d2debce3455c1897734a5607411e7ed8
dtroxler21 <dtroxler21@gmail.com>
2018-08-12 21:39:10 -0400

    adding top rated movies upon loading

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,8 @@
+$(document).ready(() => {
+    $("#movie-view").html("<p class='text-center'>Current top movies based on The Movie Database ratings (search to see what they're all about!):</p><br>");
+    nowPlayingOrTopRated("https://api.themoviedb.org/3/movie/top_rated?api_key=75f535f5e5c1d23e72fcdbe931044574&language=en-US&page=1&region=US");
+});
+
 $("#find-movie").on("click", (event) => {
 
     event.preventDefault();
@@ -13,8 +18,8 @@ $("#find-movie").on("click", (event) => {
        if (response.results[0]) {
            displayInfo(response);
        } else {
-            $("#movie-view").html("<p>No results found. Maybe you can make a new movie with that title! You can also search for these movies currently in theaters:</p><br>");
-            nowPlaying();
+            $("#movie-view").html("<p class='text-center'>No results found. Maybe you can make a new movie with that title! You can also search for these movies currently in theaters:</p><br>");
+            nowPlayingOrTopRated("https://api.themoviedb.org/3/movie/now_playing?api_key=75f535f5e5c1d23e72fcdbe931044574&language=en-US&page=1");
         }
     });
 
@@ -40,13 +45,13 @@ const displayInfo = (response) => {
     );
 }
 
-const nowPlaying = () => {
+const nowPlayingOrTopRated = (url) => {
     $.ajax({
-        url: "https://api.themoviedb.org/3/movie/now_playing?api_key=75f535f5e5c1d23e72fcdbe931044574&language=en-US&page=1",
+        url: url,
         method: "GET"
     }).then((response) => {
         const currentMovies = response.results;
-        for (var i=0; i < 3; i++) {
+        for (var i=0; i < 9; i++) {
             const posterImg = "<img src='https://image.tmdb.org/t/p/w300" + currentMovies[i].poster_path + "'>";
             const movieColumn = "<div class='col-sm-12 col-md-4'>" + posterImg + "</div>"
 
